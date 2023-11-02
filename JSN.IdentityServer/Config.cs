ec51dbbd0b4ecd230c064cff73391516b7d926de
khoant1405 <chelsea.edenhazard16@gmail.com>
2023-11-03 00:19:46 +0700

    update

--- JSN.IdentityServer/Config.cs
+++ JSN.IdentityServer/Config.cs
@@ -6,8 +6,13 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
     {
+        // Tài nguyên OpenID được sử dụng để xác định danh tính của người dùng.
         new IdentityResources.OpenId(),
+
+        // Tài nguyên Profile chứa thông tin hồ sơ của người dùng, chẳng hạn như tên và hình ảnh.
         new IdentityResources.Profile(),
+
+        // Tạo một tài nguyên tùy chỉnh có tên 'role' và gắn liên kết với tên quyền 'role'.
         new()
         {
             Name = "role",
@@ -17,42 +22,73 @@ public static class Config
 
     public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
     {
+        // Định nghĩa một phạm vi tài nguyên có tên 'JsnAPI.read'.
         new("JsnAPI.read"),
+
+        // Định nghĩa một phạm vi tài nguyên có tên 'JsnAPI.write'.
         new("JsnAPI.write")
     };
 
     public static IEnumerable<ApiResource> ApiResources => new List<ApiResource>
     {
+        // Định nghĩa một tài nguyên API có tên 'JsnAPI'.
         new("JsnAPI")
         {
+            // Xác định các phạm vi tài nguyên mà tài nguyên API này có thể sử dụng.
             Scopes = { "JsnAPI.read", "JsnAPI.write" },
+
+            // Định nghĩa các mã bí mật được sử dụng để xác thực với tài nguyên API.
             ApiSecrets = { new Secret("ScopeSecret".Sha256()) },
+
+            // Liệt kê các quyền mà người dùng có thể có khi truy cập tài nguyên API này.
             UserClaims = { "role" }
         }
     };
 
     public static IEnumerable<Client> Clients => new List<Client>
     {
+        // Định nghĩa một khách hàng (client) có tên 'm2m.client' sử dụng phương thức Client Credentials.
         new()
         {
             ClientId = "m2m.client",
             ClientName = "Client Credentials Client",
             AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // Định nghĩa mã bí mật của khách hàng.
             ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+
+            // Xác định các phạm vi mà khách hàng này được phép truy cập.
             AllowedScopes = { "JsnAPI.read", "JsnAPI.write" }
         },
+
+        // Định nghĩa một khách hàng tương tác có tên 'interactive' sử dụng phương thức Authorization Code.
         new()
         {
             ClientId = "interactive",
+
+            // Định nghĩa mã bí mật của khách hàng.
             ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+
             AllowedGrantTypes = GrantTypes.Code,
+
+            // Xác định các URI để xử lý đăng nhập và đăng xuất.
             //RedirectUris = { "https://localhost:5444/signin-oidc" },
             //FrontChannelLogoutUri = "https://localhost:5444/signout-oidc",
             //PostLogoutRedirectUris = { "https://localhost:5444/signout-callback-oidc" },
+
+            // Cho phép truy cập offline để lấy mã làm mới (refresh token).
             AllowOfflineAccess = true,
+
+            // Xác định các phạm vi mà khách hàng này được phép truy cập.
             AllowedScopes = { "openid", "profile", "JsnAPI.read" },
+
+            // Yêu cầu Proof Key for Code Exchange (PKCE) để bảo mật.
             RequirePkce = true,
+
+            // Yêu cầu sự đồng tình của người dùng (consent) khi truy cập.
             RequireConsent = true,
+
+            // Không cho phép PKCE dạng văn bản thô.
             AllowPlainTextPkce = false
         }
     };
--- JSN.IdentityServer/SeedData.cs
+++ JSN.IdentityServer/SeedData.cs
@@ -13,45 +13,61 @@ public class SeedData
 {
     public static void EnsureSeedData(string connectionString)
     {
+        // 1. Cấu hình và tạo container phụ thuộc.
         var services = ConfigureServices(connectionString);
         var serviceProvider = services.BuildServiceProvider();
 
+        // 2. Tạo một phạm vi (scope) cho container phụ thuộc.
         using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+
+        // 3. Migrate cơ sở dữ liệu của PersistedGrantDbContext.
         MigrateDatabase(scope.ServiceProvider.GetService<PersistedGrantDbContext>() ??
                         throw new InvalidOperationException());
 
+        // 4. Lấy ConfigurationDbContext để làm việc với dữ liệu cấu hình IdentityServer.
         var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
         if (context != null)
         {
+            // 5. Migrate cơ sở dữ liệu của ConfigurationDbContext.
             MigrateDatabase(context);
+
+            // 6. Thêm các clients, identity resources, API scopes và API resources.
             SeedClients(context);
             SeedIdentityResources(context);
             SeedApiScopes(context);
             SeedApiResources(context);
         }
 
+        // 7. Lấy IdentityDbContext để làm việc với dữ liệu danh tính người dùng.
         var ctx = scope.ServiceProvider.GetService<IdentityDbContext>();
         if (ctx != null)
         {
+            // 8. Migrate cơ sở dữ liệu của IdentityDbContext.
             MigrateDatabase(ctx);
         }
 
+        // 9. Đảm bảo rằng các người dùng cần thiết đã được tạo.
         EnsureUsers(scope);
     }
 
     private static IServiceCollection ConfigureServices(string connectionString)
     {
+        // 10. Tạo một bộ sưu tập dịch vụ để cấu hình và đăng ký các dịch vụ.
         var services = new ServiceCollection();
         services.AddLogging();
+
+        // 11. Thêm IdentityDbContext để làm việc với dữ liệu danh tính người dùng.
         services.AddDbContext<IdentityDbContext>(
             options => options.UseSqlServer(connectionString)
         );
 
+        // 12. Cấu hình và đăng ký các dịch vụ liên quan đến Identity, bao gồm quản lý người dùng và vai trò.
         services
             .AddIdentity<IdentityUser, IdentityRole>()
             .AddEntityFrameworkStores<IdentityDbContext>()
             .AddDefaultTokenProviders();
 
+        // 13. Thêm DbContext hoạt động cho IdentityServer (đối với các token, mã và sự đồng thuận).
         services.AddOperationalDbContext(options =>
         {
             options.ConfigureDbContext = db =>
@@ -61,6 +77,7 @@ public class SeedData
                 );
         });
 
+        // 14. Thêm DbContext cấu hình cho IdentityServer (đối với clients, identity resources, vv.).
         services.AddConfigurationDbContext(options =>
         {
             options.ConfigureDbContext = db =>
@@ -75,59 +92,73 @@ public class SeedData
 
     private static void MigrateDatabase(DbContext context)
     {
+        // 15. Migrate cơ sở dữ liệu liên quan đến DbContext đã cung cấp.
         context.Database.Migrate();
     }
 
     private static void SeedClients(ConfigurationDbContext context)
     {
+        // 16. Kiểm tra xem đã tồn tại các clients trong ConfigurationDbContext chưa.
         if (context.Clients.Any())
         {
             return;
         }
 
+        // 17. Nếu không có clients nào tồn tại, thì tạo các clients từ dữ liệu cấu hình.
         foreach (var client in Config.Clients) context.Clients.Add(client.ToEntity());
 
+        // 18. Lưu các thay đổi vào cơ sở dữ liệu.
         context.SaveChanges();
     }
 
     private static void SeedIdentityResources(ConfigurationDbContext context)
     {
+        // 19. Kiểm tra xem đã tồn tại các identity resources trong ConfigurationDbContext chưa.
         if (context.IdentityResources.Any())
         {
             return;
         }
 
+        // 20. Nếu không có identity resources nào tồn tại, thì tạo chúng từ dữ liệu cấu hình.
         foreach (var resource in Config.IdentityResources) context.IdentityResources.Add(resource.ToEntity());
 
+        // 21. Lưu các thay đổi vào cơ sở dữ liệu.
         context.SaveChanges();
     }
 
     private static void SeedApiScopes(ConfigurationDbContext context)
     {
+        // 22. Kiểm tra xem đã tồn tại các API scopes trong ConfigurationDbContext chưa.
         if (context.ApiScopes.Any())
         {
             return;
         }
 
+        // 23. Nếu không có API scopes nào tồn tại, thì tạo chúng từ dữ liệu cấu hình.
         foreach (var resource in Config.ApiScopes) context.ApiScopes.Add(resource.ToEntity());
 
+        // 24. Lưu các thay đổi vào cơ sở dữ liệu.
         context.SaveChanges();
     }
 
     private static void SeedApiResources(ConfigurationDbContext context)
     {
+        // 25. Kiểm tra xem đã tồn tại các API resources trong ConfigurationDbContext chưa.
         if (context.ApiResources.Any())
         {
             return;
         }
 
+        // 26. Nếu không có API resources nào tồn tại, thì tạo chúng từ dữ liệu cấu hình.
         foreach (var resource in Config.ApiResources) context.ApiResources.Add(resource.ToEntity());
 
+        // 27. Lưu các thay đổi vào cơ sở dữ liệu.
         context.SaveChanges();
     }
 
     private static async void EnsureUsers(IServiceScope scope)
     {
+        // 28. Đảm bảo rằng các người dùng cụ thể đã được tạo để thực hiện xác thực.
         var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
         var superAdmin = await userMgr.FindByNameAsync("superAdmin");
 
@@ -143,6 +174,7 @@ public class SeedData
             EmailConfirmed = false
         };
 
+        // 29. Tạo người dùng superAdmin.
         var result = await userMgr.CreateAsync(superAdmin, "1405");
 
         if (!result.Succeeded)
@@ -150,6 +182,7 @@ public class SeedData
             throw new Exception(result.Errors.First().Description);
         }
 
+        // 30. Thêm các quyền cho người dùng superAdmin.
         result = await userMgr.AddClaimsAsync(superAdmin, new Claim[]
         {
             new(JwtClaimTypes.Name, "superAdmin")
