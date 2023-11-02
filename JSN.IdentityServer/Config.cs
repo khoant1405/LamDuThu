2e7494d794ae44a6d6e9c0c15d9314d84d71e0f7
khoant1405 <chelsea.edenhazard16@gmail.com>
2023-11-02 23:45:25 +0700

    update

--- JSN.IdentityServer/Config.cs
+++ JSN.IdentityServer/Config.cs
@@ -2,60 +2,58 @@
 
 namespace JSN.IdentityServer;
 
-public class Config
+public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources =>
-        new[]
+    public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+        new()
         {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Profile(),
-            new IdentityResource
-            {
-                Name = "role",
-                UserClaims = new List<string> { "role" }
-            }
-        };
+            Name = "role",
+            UserClaims = new List<string> { "role" }
+        }
+    };
 
-    public static IEnumerable<ApiScope> ApiScopes =>
-        new[] { new ApiScope("JsnAPI.read"), new ApiScope("JsnAPI.write") };
+    public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+    {
+        new("JsnAPI.read"),
+        new("JsnAPI.write")
+    };
 
-    public static IEnumerable<ApiResource> ApiResources =>
-        new[]
+    public static IEnumerable<ApiResource> ApiResources => new List<ApiResource>
+    {
+        new("JsnAPI")
         {
-            new ApiResource("JsnAPI")
-            {
-                Scopes = new List<string> { "JsnAPI.read", "JsnAPI.write" },
-                ApiSecrets = new List<Secret> { new("ScopeSecret".Sha256()) },
-                UserClaims = new List<string> { "role" }
-            }
-        };
+            Scopes = { "JsnAPI.read", "JsnAPI.write" },
+            ApiSecrets = { new Secret("ScopeSecret".Sha256()) },
+            UserClaims = { "role" }
+        }
+    };
 
-    public static IEnumerable<Client> Clients =>
-        new[]
+    public static IEnumerable<Client> Clients => new List<Client>
+    {
+        new()
         {
-            // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
-                AllowedScopes = { "JsnAPI.read", "JsnAPI.write" }
-            },
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
-                AllowedGrantTypes = GrantTypes.Code,
-                RedirectUris = { "https://localhost:5444/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:5444/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:5444/signout-callback-oidc" },
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "JsnAPI.read" },
-                RequirePkce = true,
-                RequireConsent = true,
-                AllowPlainTextPkce = false
-            }
-        };
+            ClientId = "m2m.client",
+            ClientName = "Client Credentials Client",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+            AllowedScopes = { "JsnAPI.read", "JsnAPI.write" }
+        },
+        new()
+        {
+            ClientId = "interactive",
+            ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+            AllowedGrantTypes = GrantTypes.Code,
+            RedirectUris = { "https://localhost:5444/signin-oidc" },
+            FrontChannelLogoutUri = "https://localhost:5444/signout-oidc",
+            PostLogoutRedirectUris = { "https://localhost:5444/signout-callback-oidc" },
+            AllowOfflineAccess = true,
+            AllowedScopes = { "openid", "profile", "JsnAPI.read" },
+            RequirePkce = true,
+            RequireConsent = true,
+            AllowPlainTextPkce = false
+        }
+    };
 }
--- JSN.IdentityServer/SeedData.cs
+++ JSN.IdentityServer/SeedData.cs
@@ -13,140 +13,122 @@ public class SeedData
 {
     public static void EnsureSeedData(string connectionString)
     {
-        //Tạo một đối tượng ServiceCollection
+        var services = ConfigureServices(connectionString);
+        var serviceProvider = services.BuildServiceProvider();
+
+        using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        MigrateDatabase(scope, scope.ServiceProvider.GetService<PersistedGrantDbContext>());
+
+        var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+        MigrateDatabase(scope, context);
+        SeedClients(context);
+        SeedIdentityResources(context);
+        SeedApiScopes(context);
+
+        var ctx = scope.ServiceProvider.GetService<IdentityDbContext>();
+        MigrateDatabase(scope, ctx);
+        EnsureUsers(scope);
+    }
+
+    private static IServiceCollection ConfigureServices(string connectionString)
+    {
         var services = new ServiceCollection();
-
-        //Đăng ký dịch vụ ghi log.
         services.AddLogging();
-
-        //Đăng ký DbContext của IdentityDbContext để làm việc với cơ sở dữ liệu IdentityServer4.
         services.AddDbContext<IdentityDbContext>(
             options => options.UseSqlServer(connectionString)
         );
 
-        //Đăng ký dịch vụ xác thực và quản lý theo role sử dụng Identity Framework Core.
         services
             .AddIdentity<IdentityUser, IdentityRole>()
             .AddEntityFrameworkStores<IdentityDbContext>()
             .AddDefaultTokenProviders();
 
-        //Đăng ký DbContext cho thông tin vận hành của IdentityServer4.
-        services.AddOperationalDbContext(
-            options =>
-            {
-                options.ConfigureDbContext = db =>
-                    db.UseSqlServer(
-                        connectionString,
-                        sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName)
-                    );
-            }
-        );
+        services.AddOperationalDbContext(options =>
+        {
+            options.ConfigureDbContext = db =>
+                db.UseSqlServer(
+                    connectionString,
+                    sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName)
+                );
+        });
 
-        //Đăng ký DbContext cho cấu hình của IdentityServer4.
-        services.AddConfigurationDbContext(
-            options =>
-            {
-                options.ConfigureDbContext = db =>
-                    db.UseSqlServer(
-                        connectionString,
-                        sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName)
-                    );
-            }
-        );
+        services.AddConfigurationDbContext(options =>
+        {
+            options.ConfigureDbContext = db =>
+                db.UseSqlServer(
+                    connectionString,
+                    sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName)
+                );
+        });
 
-        var serviceProvider = services.BuildServiceProvider();
+        return services;
+    }
 
-        using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
-
-        //Migrates cơ sở dữ liệu cho PersistedGrantDbContext. Điều này đảm bảo rằng cơ sở dữ liệu của các phiên và mã thông báo được cập nhật đúng cách
-        scope.ServiceProvider.GetService<PersistedGrantDbContext>().Database.Migrate();
-
-        //Lấy DbContext cho cấu hình của IdentityServer4.
-        var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+    private static void MigrateDatabase(IServiceScope scope, DbContext context)
+    {
         context.Database.Migrate();
-
-        //Gọi phương thức EnsureSeedData để đảm bảo dữ liệu cấu hình được tạo nếu nó chưa tồn tại.
-        EnsureSeedData(context);
-
-        //Lấy DbContext cho quản lý người dùng và vai trò.
-        var ctx = scope.ServiceProvider.GetService<IdentityDbContext>();
-        ctx.Database.Migrate();
-
-        //Gọi phương thức EnsureUsers để đảm bảo người dùng được tạo nếu họ chưa tồn tại trong hệ thống.
-        EnsureUsers(scope);
     }
 
-    private static void EnsureUsers(IServiceScope scope)
+    private static void SeedClients(ConfigurationDbContext context)
     {
-        //Lấy UserManager để quản lý người dùng từ phạm vi dịch vụ.
-        var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-        //Tìm kiếm một người dùng với tên người dùng "superadmin" trong cơ sở dữ liệu.
-        var superadmin = userMgr.FindByNameAsync("superadmin").Result;
-
-        //Tạo mới người dùng superadmin với claim
-        if (superadmin != null)
-        {
-            return;
-        }
-
-        superadmin = new IdentityUser
-        {
-            UserName = "superAdmin",
-            Email = "vinamilk1634@gmail.com",
-            EmailConfirmed = false
-        };
-        var result = userMgr.CreateAsync(superadmin, "1405").Result;
-        if (!result.Succeeded)
-        {
-            throw new Exception(result.Errors.First().Description);
-        }
-
-        result =
-            userMgr.AddClaimsAsync(
-                superadmin,
-                new Claim[]
-                {
-                    new(JwtClaimTypes.Name, "superAdmin")
-                }
-            ).Result;
-        if (!result.Succeeded)
-        {
-            throw new Exception(result.Errors.First().Description);
-        }
-    }
-
-    private static void EnsureSeedData(ConfigurationDbContext context)
-    {
-        //Kiểm tra xem trong cơ sở dữ liệu đã có cài đặt về Clients (ứng dụng khách) hay chưa.
         if (!context.Clients.Any())
         {
-            //Nếu chưa có, thêm các cài đặt về Clients từ danh sách cài đặt Config.Clients vào cơ sở dữ liệu.
-            foreach (var client in Config.Clients.ToList()) context.Clients.Add(client.ToEntity());
+            foreach (var client in Config.Clients) context.Clients.Add(client.ToEntity());
 
             context.SaveChanges();
         }
+    }
 
+    private static void SeedIdentityResources(ConfigurationDbContext context)
+    {
         if (!context.IdentityResources.Any())
         {
-            foreach (var resource in Config.IdentityResources.ToList())
-                context.IdentityResources.Add(resource.ToEntity());
+            foreach (var resource in Config.IdentityResources) context.IdentityResources.Add(resource.ToEntity());
 
             context.SaveChanges();
         }
+    }
 
+    private static void SeedApiScopes(ConfigurationDbContext context)
+    {
         if (!context.ApiScopes.Any())
         {
-            foreach (var resource in Config.ApiScopes.ToList()) context.ApiScopes.Add(resource.ToEntity());
+            foreach (var resource in Config.ApiScopes) context.ApiScopes.Add(resource.ToEntity());
 
             context.SaveChanges();
         }
+    }
 
-        if (!context.ApiResources.Any())
+    private static async void EnsureUsers(IServiceScope scope)
+    {
+        var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var superAdmin = await userMgr.FindByNameAsync("superAdmin");
+
+        if (superAdmin == null)
         {
-            foreach (var resource in Config.ApiResources.ToList()) context.ApiResources.Add(resource.ToEntity());
+            superAdmin = new IdentityUser
+            {
+                UserName = "superAdmin",
+                Email = "vinamilk1634@gmail.com",
+                EmailConfirmed = false
+            };
 
-            context.SaveChanges();
+            var result = await userMgr.CreateAsync(superAdmin, "1405");
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = await userMgr.AddClaimsAsync(superAdmin, new Claim[]
+            {
+                new(JwtClaimTypes.Name, "superAdmin")
+            });
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
         }
     }
 }
