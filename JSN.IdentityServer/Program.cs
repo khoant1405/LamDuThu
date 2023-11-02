46be46aaf2a48abf2cef23889e584f7d631040e7
khoant1405 <chelsea.edenhazard16@gmail.com>
2023-11-03 00:00:31 +0700

    update

--- JSN.IdentityServer/Program.cs
+++ JSN.IdentityServer/Program.cs
@@ -3,25 +3,19 @@ using JSN.IdentityServer.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 var seed = args.Contains("/seed");
 if (seed)
 {
     args = args.Except(new[] { "/seed" }).ToArray();
-}
-
-var builder = WebApplication.CreateBuilder(args);
-
-var assembly = typeof(Program).Assembly.GetName().Name;
-var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-if (seed)
-{
     SeedData.EnsureSeedData(defaultConnString);
 }
 
 builder.Services.AddDbContext<IdentityDbContext>(options =>
-    options.UseSqlServer(defaultConnString,
-        b => b.MigrationsAssembly(assembly)));
+    options.UseSqlServer(defaultConnString, b => b.MigrationsAssembly(assembly)));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityDbContext>();
@@ -47,6 +41,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
-app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.Run();
